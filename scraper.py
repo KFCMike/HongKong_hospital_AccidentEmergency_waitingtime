86631b666964e4ef087ef35706f0eb5f0777ebec
KFCMike <39376007+KFCMike@users.noreply.github.com>
2018-05-25 22:53:00 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,22 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import requests
+from bs4 import BeautifulSoup
+import json
+import scraperwiki
+from datetime import datetime
+import pytz
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+req = requests.get('http://www.ha.org.hk/aedwt/data/aedWtData.json')
+j = req.json()
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+names=[j['result']['hospData'][i]['hospNameB5'] for i in range(0,18)]
+waitime=[j['result']['hospData'][i]['topWait'] for i in range(0,18)]
+names[15]=names[15][0:5]
+hkt = pytz.timezone('Asia/Hong_Kong')
+dt = datetime.now().replace(tzinfo=hkt).date()
+data = {names[i]: waitime[i] for i in range(0, 18)}
+data['date'] = dt
+print(data)
+scraperwiki.sqlite.save(unique_keys=['date'], data=data)
+
+
+
