e3316ac9d06a25b47c053f1a9f5afda7e5beb73d
KFCMike <39376007+KFCMike@users.noreply.github.com>
2018-06-26 14:47:51 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,9 +4,9 @@ import scraperwiki
 from datetime import datetime
 req = requests.get('http://www.ha.org.hk/aedwt/data/aedWtData.json')
 j = req.json()
-names=[j['result']['hospData'][i]['hospNameB5'] for i in range(0,18)]
+names=[j['result']['hospData'][i]['hospNameEn'] for i in range(0,18)]
 waitime=[j['result']['hospData'][i]['topWait'] for i in range(0,18)]
-names[15]=names[15][0:5]
+#names[15]=names[15][0:5]
 dt=j['result']['hospData'][0]['hospTimeEn']
 data = {names[i]: waitime[i] for i in range(0, 18)}
 data['date'] = dt
